d386bc9508752177e339e18831f32350e3b58dd5
Radhe0607 <krishnavkshitsagar123@gmail.com>
2026-06-09 09:47:30 +0530

    feat: create pricing environment class (fixes #3)

--- src/environment/pricing_env.py
+++ src/environment/pricing_env.py
@@ -1,2 +1,31 @@
 import gymnasium as gym
 from gymnasium import spaces
+
+
+class PricingEnvironment(gym.Env):
+    """
+    RL Environment for Dynamic Pricing
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.action_space = spaces.Discrete(5)
+
+        self.observation_space = spaces.Box(
+            low=0,
+            high=100,
+            shape=(3,),
+            dtype=float
+        )
+
+    def reset(self):
+        state = [50, 20, 10]
+        return state, {}
+
+    def step(self, action):
+        reward = 0
+        done = False
+        state = [50, 20, 10]
+
+        return state, reward, done, False, {}
